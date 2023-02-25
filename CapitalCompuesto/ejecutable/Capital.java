9959ef02364bdebf0e71dfa40247bb5d833abfbf
José Miguel Murillo Cediel <111622978+MiguelMurillo07@users.noreply.github.com>
2023-02-25 09:26:27 -0500

    Update Capital.java

--- CapitalCompuesto/ejecutable/Capital.java
+++ CapitalCompuesto/ejecutable/Capital.java
@@ -6,32 +6,26 @@ public class Capital
 {
     public static void main(String[] args) {
         /* Declaración de variables */
-            double c1, c2, c3, k, m, b, d;
+            double c1, c2, c3, k, b, d;
 
         // Input
         c1 = Double.parseDouble(JOptionPane.showInputDialog("Digite el capital de Pedro: "));
-        c2 =Double.parseDouble(JOptionPane.showInputDialog("Digite el capital de Juan: "));
-        c3 = 0;
+        c2 = Double.parseDouble(JOptionPane.showInputDialog("Digite el capital de Juan: "));
+        c3 = Double.parseDouble(JOptionPane.showInputDialog("Digite el capital del negocio: "));
         k = 0;
-        m = 0;
+        b = c1 + c2;
 
         // Processing and Output
-        b = 2*c1;
-        d = 2*c2;
 
-        while (c1 <= b){
+        while (b <= c3){
             c1 = ((c1*0.03)+c1);
+            c2 = ((c2*0.04)+c2);
+            b = c1 + c2;
             k = k + 1;
         }
-        JOptionPane.showMessageDialog(null, "El capital de Pedro se duplicará luego de "+k+ " meses.", "Capital Compuesto", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "El capital de Pedro y Juan será viable para el negocio luego de "+k+ " meses.", "Capital Compuesto", JOptionPane.INFORMATION_MESSAGE);
 
-        while (c2 <= d){
-            c2 = ((c2*0.04)+c2);
-            m = m + 1;
-        }
-        JOptionPane.showMessageDialog(null, "El capital de Juan se duplicará luego de "+m+ " meses.", "Capital Compuesto", JOptionPane.INFORMATION_MESSAGE);
-
-
+        
         // Fin
         System.exit(0);
     }
